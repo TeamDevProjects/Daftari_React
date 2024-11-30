dc70a0408035e7de0a537b8ce79bf1474560e9e7
KhalilArfaoui41 <khalil.rayen.k@gmail.com>
2024-11-30 14:10:05 +0100

    i install some library and i configure there codes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import 'react-toastify/dist/ReactToastify.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import './index.css'
+import { ToastContainer } from 'react-toastify'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ToastContainer position="top-center" autoClose={2000} />
     <App />
-  </StrictMode>,
+  </React.StrictMode>
 )
