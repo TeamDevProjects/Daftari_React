2785269b3bc26a87daded2bce06f43b6e3f93980
Ahmedmeid99 <ahmedalbakrycool@gmail.com>
2024-12-03 12:16:00 +0200

    update signin property

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,6 +1,17 @@
 /* eslint-disable react/prop-types */
 // eslint-disable-next-line react/prop-types
-const FormInput = ({ label, name, type, defaultValue, size }) => {
+const FormInput = ({
+  label,
+  name,
+  type,
+  defaultValue,
+  size,
+  required,
+  Error,
+  TextError,
+  onChange,
+  onKeyDown,
+}) => {
   return (
     <div className="form-control">
       <label htmlFor={name} className="label">
@@ -10,7 +21,10 @@ const FormInput = ({ label, name, type, defaultValue, size }) => {
         type={type}
         name={name}
         defaultValue={defaultValue}
-        className={`input input-bordered ${size}`}
+        className={`input input-bordered ${Error && 'input-error'} ${size}`}
+        onChange={onChange}
+        onKeyDown={onKeyDown}
+        required={required}
       />
       {Error && <p className="text-error">{TextError}</p>}
     </div>
