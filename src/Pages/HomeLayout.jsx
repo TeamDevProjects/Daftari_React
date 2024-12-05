0826051e505a74d7a4f42dfdd0ce7f234d4e8e0e
Ahmedmeid99 <ahmedalbackrycool@gmail.com>
2024-12-05 18:53:18 +0200

    hadel redir to signin if no user exist

--- src/Pages/HomeLayout.jsx
+++ src/Pages/HomeLayout.jsx
@@ -1,20 +1,25 @@
 import { Outlet, useNavigation, useNavigate } from 'react-router-dom'
 import { Navbar } from '../components'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import userServices from '../Services/user'
 
 import authService from '../Services/authService'
 import React from 'react'
 const HomeLayout = () => {
   const navigation = useNavigation()
-
+  const [isUser, setIsUser] = useState(false)
   const isPageLoading = navigation.state === 'loading'
   // is login before in localstorage and token is valid redirect to page
   const navigate = useNavigate()
 
   const isTokenValid = () => {
     const token = authService.getAccessToken()
-    if (!token) return false
+    if (!token) {
+      setIsUser(false)
+      return false
+    }
+
+    setIsUser(true)
 
     return true
   }
@@ -25,6 +30,11 @@ const HomeLayout = () => {
     }
     console.log(userServices.GetClients())
   }, [navigate])
+
+  if (!isUser) {
+    return <div className="loading" />
+  }
+
   return (
     <>
       <Navbar />
